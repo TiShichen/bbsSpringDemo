6c804b7ec4bc311812abab6b14955b6408733e75
TiShichen <acemichael2015@gmail.com>
2019-08-15 15:57:53 +0800

    updated pagination

--- src/main/java/demo/springboot/community/bbs/service/QuestionService.java
+++ src/main/java/demo/springboot/community/bbs/service/QuestionService.java
@@ -28,13 +28,24 @@ public class QuestionService {
     private UserMapper userMapper;
 
     public PaginationDTO list(Integer page, Integer size) {
+
+        PaginationDTO paginationDTO = new PaginationDTO();
+        Integer totalCount = questionMapper.count();
+        paginationDTO.setPagination(totalCount, page, size);
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > paginationDTO.getTotalPage()) {
+            page = paginationDTO.getTotalPage();
+        }
+
         // 5 * (i - 1) ==> size * (page - 1)
         Integer offset = size * (page - 1);
 
         List<Question> questions = questionMapper.list(offset, size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
 
-        PaginationDTO paginationDTO = new PaginationDTO();
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -43,9 +54,8 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
+
         paginationDTO.setQuestions(questionDTOList);
-        Integer totalCount = questionMapper.count();
-        paginationDTO.setPagination(totalCount, page, size);
         return paginationDTO;
     }
 }
