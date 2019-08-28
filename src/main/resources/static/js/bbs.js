37be63393d3ea094f9f3cf3e71b83e6f948f7d77
TiShichen <acemichael2015@gmail.com>
2019-08-28 11:53:39 +0800

    added login function when commenting

--- src/main/resources/static/js/bbs.js
+++ src/main/resources/static/js/bbs.js
@@ -3,13 +3,28 @@ function post() {
     var content = $("#comment_area").val();
     $.ajax({
         type: "POST",
-        url: url,
-        data: data,
+        url: "/comment",
+        contentType: 'application/json',
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": content,
+            "type": 1
+        }),
         success: function (response) {
-            console.log(response);
+            if (response.code == 200) {
+                $("#comment_section").hide();
+            } else {
+                if (response.code == 2003) {
+                    var isAccepted = confirm("Press confirm to proceed to login, please allow pop-up." + "\n\n" + response.message);
+                    if (isAccepted) {
+                        window.open("https://github.com/login/oauth/authorize?client_id=30db3d189a78ee7c7558&redirect_uri=http://localhost:8887/callback&scope=user&state=1");
+                        window.localStorage.setItem("closable", true);
+                    }
+                } else {
+                    alert(response.message);
+                }
+            }
         },
         dataType: "json"
     });
-    console.log(questionId);        // = System.out.print()
-    console.log(content);
 }
